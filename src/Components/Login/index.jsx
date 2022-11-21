535deabc2b01dc8d9c8430bbef78f52f09374b5a
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2022-11-21 14:25:39 +0500

    cart

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -1,19 +1,41 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import "../../Root/App.css"
 
-const Login = () => {
+const Login = ({token,setToken}) => {
     const [userName, setUsername] = useState('')
-    const [userPassword, setUserPassword] = useState('')
+    const [password, setUserPassword] = useState('')
     const [error, setErro] = useState('')
+    const loginHandeler = () => {
+        setUsername('')
+        setUserPassword('')
+        setErro('')
 
+        axios({
+            url: "https://fakestoreapi.com/auth/login",
+            method: "POST",
+            data: {
+                username: userName,
+                password: password
+          }
+        }).then(res => {
+            console.log(res.data.token, 'ruqi');
+            setToken(res.data.token)
+            localStorage.setItem("userToken",res.data.token)
+        }).catch(err => {
+            console.log(err.response.data);
+            setErro(err.response.data)
+    })
+}
   return (
       <div className='Login'>
           <div className='Login-inputs'>
+              <p>name: mor_2314</p>
+              <p> password: 83r5^_</p>
               <input type="text" value={userName} onChange={(e)=>setUsername(e.target.value)} placeholder="UserName" />
-              <input type="password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)}  placeholder="Password"/>
-
-              <small>this is error placeholder</small>
-              <button>Login </button>
+              <input type="password" value={password} onChange={(e) => setUserPassword(e.target.value)}  placeholder="Password"/>
+              {error && <small> {error} </small>}
+              <button onClick={loginHandeler}>Login </button>
           </div>
     </div>
   )
--- src/Components/Product/inde.jsx
+++ src/Components/Product/inde.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Product = () => {
+  const [datas, setData] = useState([]);
+  
+
+   
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((res) => {
+      
+        setData(res || []);
+      });
+  }, []);
+  console.log(datas,'ruqiya');
+
+
   return (
-    <div>Product</div>
+    <div className='ProductCon'>
+      {datas?.map((val) => {
+    return ( <div className='Card' key={val.id}>
+     
+    </div>)
+      })}
+    </div>
+ 
   )
 }
 export default Product
